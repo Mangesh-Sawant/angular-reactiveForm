49fdcaeb48887c9821222ec371b07655463d0498
mangesh sawant <mangesh.sawant@ilmtec.in>
2024-07-10 00:42:38 +0530

    add form group

--- reactive-form/src/app/app.component.ts
+++ reactive-form/src/app/app.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatOption, MatSelect} from "@angular/material/select";
+import {MatInput} from "@angular/material/input";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatFormField, MatSelect, MatOption, MatInput, MatLabel, ReactiveFormsModule, MatButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
-export class AppComponent {
-  title = 'reactive-form';
+export class AppComponent implements OnInit {
+  loginForm!: FormGroup;
+  addressForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.setLoginFormFromGroup();
+  }
+
+
+  ngOnInit(): void {
+    this.onChangeFormValue();
+  }
+
+  submit(): void {
+    console.log(this.loginForm.value);
+  }
+
+  private setLoginFormFromGroup(): void {
+    this.loginForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      gender: ['', [Validators.required]],
+      info: ['', [Validators.required]],
+      address: this.addressForm = this.formBuilder.group({
+        address: ['', [Validators.required]]
+      })
+    });
+  }
+
+  private onChangeFormValue(): void {
+    this.loginForm?.valueChanges.subscribe(val => {
+      console.log(val);
+    });
+  }
 }
